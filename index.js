f07097bc6e93004bec6f58aaed6127dac7518d37
victorevh <victor_oliveirasantos@hotmail.com>
2022-12-14 15:35:14 -0300

    Adicciona addRow

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ const getDoc = async () => {
 getDoc().then(doc => {
     console.log(doc.title);
 });
+
+let sheet; 
+getDoc().then(doc => {
+    sheet = doc.sheetsByIndex[0];
+    sheet.addRow({
+        nome: "Victor Oliveira",
+        idade: 26,
+        email: "victorevh@gmail.com"
+    }).then(() => {
+        console.log('dado salvo!')
+    })
+});
